1dca326ef23b692d7f017e3ec8917248773b41eb
Michael <eoloneox@gmail.com>
2021-08-16 09:50:56 +0200

    mCI adding table cponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
